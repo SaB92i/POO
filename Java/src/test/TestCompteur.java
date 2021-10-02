bed34c095f583ad1feccabc9bc86e74572282a31
sabry <sab92222@gmail.com>
2021-10-02 17:06:01 +0200

    second commit

--- Java/src/test/TestCompteur.java
+++ Java/src/test/TestCompteur.java
@@ -8,7 +8,7 @@ public class TestCompteur{
             c1.incrementer();
             System.out.println(c1);
         }
-        for (int i=0;i<20;i--){
+        for (int i=0;i<20;i++){
             c1.decrementer();
             System.out.println(c1);
         }
